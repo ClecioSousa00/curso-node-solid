487f979a559edb3ed5c8d3440e7a1830a7206cb6
ClecioSousa00 <cleciosouza015@gmail.com>
2024-10-28 20:25:02 -0300

    test: register use case

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,27 +1,37 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
-type Register = {
+interface Register {
   name: string
   email: string
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ email, name, password }: Register) {
+  async execute({
+    email,
+    name,
+    password,
+  }: Register): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) throw new UserAlreadyExistsError()
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       email,
       name,
       password_hash,
     })
+    return { user }
   }
 }
